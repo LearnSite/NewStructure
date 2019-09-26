1acb4323dab9fa25d21c9a81238066ea0aebdb97
Serhii <ser.shulyma@gmail.com>
2019-09-26 13:06:46 +0300

    Temporary version

---
+++ web/themes/custom/new_structure/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.new_structure = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
